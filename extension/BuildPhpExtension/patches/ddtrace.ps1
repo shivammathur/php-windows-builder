d7ce192620c1699f24a57e996301a84c98989481
Shivam Mathur <contact@shivammathur.com>
2026-05-21 15:24:10 +0530

    Fix datadog_trace Windows build

---
+++ extension/BuildPhpExtension/patches/ddtrace.ps1
@@ -0,0 +1,6 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/dd-trace-php/master/ddtrace-windows.sym" -OutFile "ddtrace-windows.sym"
+Invoke-WebRequest -Uri "https://github.com/DataDog/libdatadog/archive/refs/heads/main.zip" -OutFile "$ENV:TEMP/libdatadog.zip"
+Remove-Item -Path "libdatadog" -Recurse -Force
+Expand-Archive -Path "$ENV:TEMP/libdatadog.zip" -DestinationPath "." -Force
+Rename-Item -Path "libdatadog-main" -NewName "libdatadog"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/libdatadog/main/libdd-common/src/cc_utils.rs" -OutFile "libdatadog/tools/cc_utils/src/lib.rs"
