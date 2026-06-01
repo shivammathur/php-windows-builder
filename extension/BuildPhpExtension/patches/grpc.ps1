63729821e1be1d8ac9404ab02e8bf49cdfde9acf
Shivam Mathur <shivam_jpr@hotmail.com>
2026-06-01 20:11:44 +0530

    Fix grpc build with legacy MSVC preprocessor

--- extension/BuildPhpExtension/patches/grpc.ps1
+++ extension/BuildPhpExtension/patches/grpc.ps1
@@ -10,7 +10,7 @@ function CreateFolderIfMissing(path) {
 "@
 (Get-Content config.w32) | ForEach-Object { $_.Replace('base_dir+"\\ext\\grpc', 'base_dir+"') } | Set-Content config.w32
 (Get-Content config.w32) | ForEach-Object { $_.Replace('FSO.CreateFolder', 'CreateFolderIfMissing') } | Set-Content config.w32
-(Get-Content config.w32) | ForEach-Object { $_ -replace '/D_WIN32_WINNT=0x600', '/D_WIN32_WINNT=0x600 /FS /std:c++17' } | Set-Content config.w32
+(Get-Content config.w32) | ForEach-Object { $_ -replace '/D_WIN32_WINNT=0x600', '/D_WIN32_WINNT=0x600 /FS /std:c++17 /Zc:preprocessor' } | Set-Content config.w32
 (Get-Content config.w32) | ForEach-Object { $_.Replace('ARG_WITH("grpc", "grpc support", "no");', $fn) } | Set-Content config.w32
 
 $wrapperPath = "src\php\ext\grpc\php7_wrapper.h"
