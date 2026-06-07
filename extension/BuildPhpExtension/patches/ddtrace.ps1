c1fb312b99592b4c9376a54ad4b13ace45bf555d
Shivam Mathur <shivam_jpr@hotmail.com>
2026-06-07 22:24:02 +0530

    Fix filename in datadog_trace patch

--- extension/BuildPhpExtension/patches/ddtrace.ps1
+++ extension/BuildPhpExtension/patches/ddtrace.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/dd-trace-php/master/ddtrace-windows.sym" -OutFile "ddtrace-windows.sym"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/dd-trace-php/master/datadog-windows.sym" -OutFile "datadog-windows.sym"
 Invoke-WebRequest -Uri "https://github.com/DataDog/libdatadog/archive/refs/heads/main.zip" -OutFile "$ENV:TEMP/libdatadog.zip"
 Remove-Item -Path "libdatadog" -Recurse -Force
 Expand-Archive -Path "$ENV:TEMP/libdatadog.zip" -DestinationPath "." -Force
