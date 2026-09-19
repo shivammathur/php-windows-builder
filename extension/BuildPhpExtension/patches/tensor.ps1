1a8617b67f2aa2fd7bf7dce581de61b08301da9b
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-19 10:13:01 +0530

    Fix Tensor OpenBLAS configuration

--- extension/BuildPhpExtension/patches/tensor.ps1
+++ extension/BuildPhpExtension/patches/tensor.ps1
@@ -1,1 +1,18 @@
-Invoke-Webrequest -Uri https://raw.githubusercontent.com/RubixML/Tensor/9e9281957fb9223122d1ef77bf5343897492225d/ext/config.w32 -OutFile ext\config.w32
+$configPath = Get-RecursiveFilePath -Directory (Get-Location).Path -FileName 'config.w32'
+$config = Get-Content -LiteralPath $configPath -Raw
+
+if ($config -notmatch 'CHECK_LIB\(\s*"libopenblas\.lib"') {
+    $guard = 'if (PHP_TENSOR != "no") {'
+    if (-not $config.Contains($guard)) {
+        throw 'Unable to find the Tensor enable guard in config.w32'
+    }
+    $lineEnding = if ($config.Contains("`r`n")) { "`r`n" } else { "`n" }
+    $dependencyGuard = @(
+        '  if (!CHECK_LIB("libopenblas.lib", "tensor", PHP_TENSOR) ||',
+        '      !CHECK_HEADER_ADD_INCLUDE("cblas.h", "CFLAGS_TENSOR")) {',
+        '    ERROR("tensor requires OpenBLAS libraries and headers");',
+        '  }'
+    ) -join $lineEnding
+    $config = $config.Replace($guard, $guard + $lineEnding + $dependencyGuard)
+    Set-Content -LiteralPath $configPath -Value $config -Encoding utf8 -NoNewline
+}
