72feab20bd44f04d947dc60fbf208d48bcd70c82
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 11:07:36 +0530

    Test PHP builds with SDK master

--- php/BuildPhp/private/Get-PhpSdk.ps1
+++ php/BuildPhp/private/Get-PhpSdk.ps1
@@ -7,7 +7,7 @@ function Get-PhpSdk {
     param (
     )
     begin {
-        $sdkVersion = "9153ac9355bb12a1cac6751fec7cedd9bcf8d187"
+        $sdkVersion = "master"
         $url = "https://github.com/php/php-sdk-binary-tools/archive/$sdkVersion.zip"
     }
     process {
